2d7a531328d9232c17395837dbf53fa4e1408962
Flugerops <bogdan07112009@gmail.com>
2024-03-13 19:16:46 +0200

    Homework

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 app.template_folder = "../templates/"
+app.static_folder = "../static/"
 
 from . import routes
 
 
-if __name__ == "__main__":
-    app.run(port=8000)
--- app/routes/default.py
+++ app/routes/default.py
@@ -3,9 +3,9 @@ from flask import render_template
 
 @app.route("/")
 def index():    
-    return "<h1>HELLO, ZMIINI NOVATORI</h1>"
+    return render_template("_index.html")
 
 @app.route("/menu")
-def index_app1():
+def menu():
 
-    return render_template("_index.html")
+    return render_template("menu.html")
